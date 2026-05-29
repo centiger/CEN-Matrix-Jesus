929b29102f84f3f73282d91a34b1c1d5f24ac473
centiger <centiger2@gmail.com>
2026-05-30 00:34:50 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='jesus-matrix-v1-20260530-resurrection';
+const CACHE_NAME='jesus-matrix-v1-20260530-resurrection-realfix';
 const ASSETS=['./','./index.html','./manifest.json','./hubs/index.html','./hubs/assets/maps/incarnation-hub-map.png','./hubs/assets/maps/ministry-hub-map.png','./hubs/assets/maps/revelation-hub-map.png','./hubs/assets/maps/cross-hub-map.png','./hubs/assets/maps/resurrection-hub-map.png','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)).catch(()=>{}));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
